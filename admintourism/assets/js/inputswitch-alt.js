bf31251bfca1145e53ea33c50f44341d6680a4ba
nishant-taletiya <86609923+nishant-taletiya@users.noreply.github.com>
2023-06-03 08:34:46 +0530

    delete

--- admintourism/assets/js/inputswitch-alt.js
+++
@@ -1,6 +0,0 @@
-$(function() { 
-   "use strict";                      //run when the DOM is ready
-  $('.switch-toggle').on('click', function(){
-    $(this).toggleClass("switch-on");
-});
-});
--- application/models/restaurantmodel.php
+++
@@ -1,9 +0,0 @@
-<?php
-class restaurantmodel extends CI_Model 
-{
-	public function getrestaurant()
-	{
-	return $this->db->get('restaurant')->result();
-	}
-}
-?>	
